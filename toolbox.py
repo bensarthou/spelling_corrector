7df50ab5f42c3dd7a65ca47d09e94809f5576825
Cadart Nicolas <nicolas.cadart@ensta-paristech.fr>
2018-11-22 11:46:31 +0100

    [*] Add doc and reformat all jupyter notebook to fit to the project. Add dataset parsing

--- toolbox.py
+++ toolbox.py
@@ -1,5 +1,17 @@
 import random
 import numpy as np
+import pickle
+
+
+def load_db(dir="data", error_rate=10):
+
+    f_train = open("{}/train{}.pkl".format(dir, error_rate), 'rb')
+    db_train = pickle.load(f_train)
+
+    f_test = open("{}/test{}.pkl".format(dir, error_rate), 'rb')
+    db_test = pickle.load(f_test)
+
+    return db_train, db_test
 
 
 def get_train_validation_test_sets(data, sets_ratio=(0.8, 0.1, 0.1)):
