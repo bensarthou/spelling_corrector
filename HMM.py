e3a3eec53bc15b49b143e6e2ed66e384544cdf6a
Cadart Nicolas <nicolas.cadart@ensta-paristech.fr>
2018-12-16 12:15:43 +0100

    [*] Add verbose param to HMM, inheritate HMM2 from HMM, accelerate sequences conversions with numpy arrays

--- HMM.py
+++ HMM.py
@@ -30,10 +30,7 @@ def get_observations_states(X, y, min_observation_count=0):
 
 
 class HMM:
-    def __init__(self, state_list, observation_list,
-                 transition_proba = None,
-                 observation_proba = None,
-                 initial_state_proba = None):
+    def __init__(self, state_list, observation_list, verbose = True):
         """
         Builds a 1st order Hidden Markov Model
         state_list is the list of state symbols [s_0...s_(N-1)]
@@ -49,24 +46,17 @@ class HMM:
         self.omega_X = sorted(list(set(observation_list)))  # Keep the vocabulary of observations
         self.n_states = len(state_list)               # The number of states
         self.n_observations = len(observation_list)   # The number of observations
+        self.verbose = verbose
 
-        print("1st order HMM created with: ")
-        print(" * {} states".format(self.n_states))
-        print(" * {} observations".format(self.n_observations))
+        if self.verbose:
+            print("1st order HMM created with: ")
+            print(" * {} states".format(self.n_states))
+            print(" * {} observations".format(self.n_observations))
 
         # Init. of the 3 distributions : observation, transition and initial states
-        if transition_proba is None:
-            self.transition_proba = np.zeros( (self.n_states, self.n_states), float)
-        else:
-            self.transition_proba = transition_proba
-        if observation_proba is None:
-            self.observation_proba = np.zeros( (self.n_states, self.n_observations), float)
-        else:
-            self.observation_proba = observation_proba
-        if initial_state_proba is None:
-            self.initial_state_proba = np.zeros( (self.n_states,), float )
-        else:
-            self.initial_state_proba = initial_state_proba
+        self.transition_proba = np.zeros( (self.n_states, self.n_states), float)
+        self.observation_proba = np.zeros( (self.n_states, self.n_observations), float)
+        self.initial_state_proba = np.zeros( (self.n_states,), float )
 
         # Since everything will be stored in numpy arrays, it is more convenient and compact to
         # handle words and tags as indices (integer) for a direct access. However, we also need
@@ -80,15 +70,17 @@ class HMM:
         :param X: list of observations sequences. Ex: [['o1', 'o2', 'o3'], ['o1', 'o2']]
         :param y: list of states sequences. Ex: [['s1', 's2', 's3'], ['s1', 's2']]
         """
-        print("Training initial states probabilities...", end="")
+        if self.verbose:
+            print("Training initial states probabilities...", end="")
         self.train_initstate_proba(y)
-        print(" Done.")
-        print("Training transitions probabilities given states...", end="")
+        if self.verbose:
+            print(" Done.\nTraining transitions probabilities given states...", end="")
         self.train_transitions_proba(y)
-        print(" Done.")
-        print("Training observations probabilities given states...", end="")
+        if self.verbose:
+            print(" Done.\nTraining observations probabilities given states...", end="")
         self.train_observations_proba(X, y)
-        print(" Done.")
+        if self.verbose:
+            print(" Done.")
 
 
     def train_initstate_proba(self, y):
@@ -187,9 +179,9 @@ class HMM:
         :param observations_sequences: list of observations sequences. Ex: [['o1', 'o2', 'o3'], ['o1', 'o2']]
         :return states_sequences: list of states sequences. Ex: [['s1', 's2', 's3'], ['s1', 's2']]
         """
-        states_sequences = []
-        for obs_seq in observations_sequences:
-            states_sequences.append(self.viterbi(obs_seq))
+        states_sequences = [None] * len(observations_sequences)
+        for i_seq, obs_seq in enumerate(observations_sequences):
+            states_sequences[i_seq] = self.viterbi(obs_seq)
         return states_sequences
 
 
@@ -239,22 +231,22 @@ class HMM:
     def _convert_observations_sequence_to_index(self, observations_sequence):
         """
         Convert sequence of observations to sequence of numerical index
-        :param observations_sequence: sequence of observations. Ex: [['o1', 'o2', 'o3'], ['o1', 'o2']]
-        :return: obs_seq: numerical encoded observations sequence Ex: [[1, 2, 3], [1, 2]]
+        :param observations_sequence: sequence of observations. Ex: ['o1', 'o2', 'o3']
+        :return: obs_seq: numerical encoded observations sequence Ex: [1, 2, 3]
         """
-        obs_seq = []
-        for obs in observations_sequence:
+        obs_seq = np.zeros(len(observations_sequence), int)
+        for i_obs, obs in enumerate(observations_sequence):
             if obs not in self.X_index.keys():
                 obs = UNK
-            obs_seq.append(int(self.X_index[obs]))
+            obs_seq[i_obs] = self.X_index[obs]
         return obs_seq
 
 
     def _convert_states_sequence_to_string(self, states_seq):
         """
         Convert numerical sequence of states to sequence of strings
-        :param states_seq: numerical encoded states sequence. Ex: [[1, 2, 3], [1, 2]]
-        :return: states_sequence: numerical encoded observations sequence. Ex: [['s1', 's2', 's3'], ['s1', 's2']]
+        :param states_seq: numerical encoded states sequence. Ex: [1, 2, 3]
+        :return: states_sequence: numerical encoded observations sequence. Ex: ['s1', 's2', 's3']
         """
         states_sequence = []
         for i_state in states_seq:
@@ -262,8 +254,8 @@ class HMM:
         return states_sequence
 
 
-class HMM2:
-    def __init__(self, state_list, observation_list):
+class HMM2(HMM):
+    def __init__(self, state_list, observation_list, verbose = True):
         """
         Builds a 2nd order Hidden Markov Model
         state_list is the list of state symbols [s_0...s_(N-1)]
@@ -279,10 +271,12 @@ class HMM2:
         self.omega_X = sorted(list(set(observation_list)))  # Keep the vocabulary of observations
         self.n_states = len(state_list)               # The number of states
         self.n_observations = len(observation_list)   # The number of observations
+        self.verbose = verbose
 
-        print("2nd order HMM created with: ")
-        print(" * {} states".format(self.n_states))
-        print(" * {} observations".format(self.n_observations))
+        if self.verbose:
+            print("2nd order HMM created with: ")
+            print(" * {} states".format(self.n_states))
+            print(" * {} observations".format(self.n_observations))
 
         # Init. of the 3 distributions : observation, transition and initial states
         self.initial_state_proba = np.zeros( (self.n_states,), float)
@@ -302,90 +296,17 @@ class HMM2:
         :param X: list of observations sequences. Ex: [['o1', 'o2', 'o3'], ['o1', 'o2']]
         :param y: list of states sequences. Ex: [['s1', 's2', 's3'], ['s1', 's2']]
         """
-        print("Training initial states probabilities...", end="")
+        if self.verbose:
+            print("Training initial states probabilities...", end="")
         self.train_initstate_proba(y)
-        print(" Done.")
-        print("Training transitions probabilities given states...", end="")
+        if self.verbose:
+            print(" Done.\nTraining transitions probabilities given states...", end="")
         self.train_transitions_proba(y, smoothing=smoothing)
-        print(" Done.")
-        print("Training observations probabilities given states...", end="")
+        if self.verbose:
+            print(" Done.\nTraining observations probabilities given states...", end="")
         self.train_observations_proba(X, y)
-        print(" Done.")
-
-
-    def predict(self, observations_sequences):
-        """
-        Predict the sequences of states from sequences of observations.
-        :param observations_sequences: list of observations sequences. Ex: [['o1', 'o2', 'o3'], ['o1', 'o2']]
-        :return states_sequences: list of states sequences. Ex: [['s1', 's2', 's3'], ['s1', 's2']]
-        """
-        states_sequences = []
-        for obs_seq in observations_sequences:
-            states_sequences.append(self.viterbi(obs_seq))
-        return states_sequences
-
-
-    def score(self, X, y, ignore_unk=True):
-        """
-        Run predictions on each observation sequence of the dataset and return accuracy score.
-        :param X: list of observations sequences. Ex: [['o1', 'o2', 'o3'], ['o1', 'o2']]
-        :param y: list of states sequences. Ex: [['s1', 's2', 's3'], ['s1', 's2']]
-        :return: accuracy_tokens, accuracy_sequences: accuracy rates of predictions at tokens or sequences levels
-        """
-        true_predictions = 0
-        total_predictions = 0
-        true_sequences = 0
-        for obs_seq, states_seq in zip(X, y):
-            # run prediction
-            pred_states_seq = self.viterbi(obs_seq)
-
-            # if UNK are ignored, remove their predictions
-            if ignore_unk:
-                states_seq      = [states_seq[t]      for t in range(len(obs_seq)) if obs_seq[t] in self.X_index.keys()]
-                pred_states_seq = [pred_states_seq[t] for t in range(len(obs_seq)) if obs_seq[t] in self.X_index.keys()]
-
-            # check prediction
-            true_predictions += np.sum([states_seq[t] == pred_states_seq[t] for t in range(len(states_seq))])
-            total_predictions += len(states_seq)
-            true_sequences += (states_seq == pred_states_seq)
-
-        accuracy_tokens = true_predictions / total_predictions
-        accuracy_sequences = true_sequences / len(y)
-
-        return accuracy_tokens, accuracy_sequences
-
-
-    def train_initstate_proba(self, y):
-        """
-        Estimate initial states probabilities P(Y(0)) from states sequences.
-        :param y: list of states sequences. Ex: [['s1', 's2', 's3'], ['s1', 's2']]
-        """
-        states_counts = Counter([state_seq[0] for state_seq in y])
-        total_counts = np.sum(list(states_counts.values()))
-        for state in self.omega_Y:
-            self.initial_state_proba[self.Y_index[state]] = states_counts[state] / total_counts
-
-
-    def train_observations_proba(self, X, y):
-        """
-        Estimate observations probabilities given states, P(X(t)|Y(t)).
-        :param X: list of observations sequences. Ex: [['o1', 'o2', 'o3'], ['o1', 'o2']]
-        :param y: list of states sequences. Ex: [['s1', 's2', 's3'], ['s1', 's2']]
-        """
-        # reset observation matrix
-        self.observation_proba = np.zeros((self.n_states, self.n_observations), float)
-
-        # get counts
-        for obs_seq, states_seq in zip(X, y):
-            for obs, state in zip(obs_seq, states_seq):
-                # check if observation is known
-                if obs not in self.X_index.keys():
-                    obs = UNK
-                # update counts
-                self.observation_proba[self.Y_index[state], self.X_index[obs]] += 1
-
-        # normalize observation proba (normalize each line to 1)
-        self.observation_proba /= np.atleast_2d(np.sum(self.observation_proba, axis=1)).T
+        if self.verbose:
+            print(" Done.")
 
 
     def train_transitions_proba(self, y, smoothing=True):
@@ -489,7 +410,7 @@ class HMM2:
                     path_table[i_state, j_state, t] = np.argmax(p_state_given_prev_states_and_obs)
 
         # back-tracking of optimal states sequence
-        states_seq = np.zeros((n_time,), int)
+        states_seq = np.zeros(n_time, int)
         states_seq[-2], states_seq[-1] = np.unravel_index(np.argmax(prob_table[:, :, -1]), prob_table[:, :, -1].shape)
         for t in reversed(range(2, n_time)):
             states_seq[t-2] = path_table[states_seq[t-1], states_seq[t], t]
@@ -497,43 +418,4 @@ class HMM2:
         # ----- CONVERSION OF INDEXES TO REAL STATES
         states_sequence = self._convert_states_sequence_to_string(states_seq)
 
-        return states_sequence
-
-
-    def _make_indexes(self):
-        """
-        Creates the reverse table that maps states/observations names
-        to their index in the probabilities arrays
-        """
-        self.Y_index = {}
-        for i in range(self.n_states):
-            self.Y_index[self.omega_Y[i]] = i
-        self.X_index = {}
-        for i in range(self.n_observations):
-            self.X_index[self.omega_X[i]] = i
-
-
-    def _convert_observations_sequence_to_index(self, observations_sequence):
-        """
-        Convert sequence of observations to sequence of numerical index
-        :param observations_sequence: sequence of observations. Ex: [['o1', 'o2', 'o3'], ['o1', 'o2']]
-        :return: obs_seq: numerical encoded observations sequence Ex: [[1, 2, 3], [1, 2]]
-        """
-        obs_seq = []
-        for obs in observations_sequence:
-            if obs not in self.X_index.keys():
-                obs = UNK
-            obs_seq.append(int(self.X_index[obs]))
-        return obs_seq
-
-
-    def _convert_states_sequence_to_string(self, states_seq):
-        """
-        Convert numerical sequence of states to sequence of strings
-        :param states_seq: numerical encoded states sequence. Ex: [[1, 2, 3], [1, 2]]
-        :return: states_sequence: numerical encoded observations sequence. Ex: [['s1', 's2', 's3'], ['s1', 's2']]
-        """
-        states_sequence = []
-        for i_state in states_seq:
-            states_sequence.append(self.omega_Y[int(i_state)])
         return states_sequence
